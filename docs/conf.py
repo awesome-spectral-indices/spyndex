bbba98a8a4033f6f1bddba2cce37c534000f9ebe
davemlz <dml.mont@gmail.com>
2021-09-09 21:53:12 +0200

    Docs updated

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
 
 # -- Project information -----------------------------------------------------
 
