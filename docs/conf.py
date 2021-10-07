db03340f5a21922fe14f6da87e58d879b60932b6
davemlz <dml.mont@gmail.com>
2021-10-07 21:11:43 +0200

    v0.0.1 -> v0.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = "2021, David Montero Loaiza"
 author = "David Montero Loaiza"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.0.2"
 
 
 # -- General configuration ---------------------------------------------------
