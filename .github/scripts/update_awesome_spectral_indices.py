6ddee884def5de727bc1657eea8d0ab88dde6f31
David Montero Loaiza <49817852+davemlz@users.noreply.github.com>
2022-10-08 14:07:21 +0200

    FEAT: Updated workflow to update bands and constants

--- .github/scripts/update_awesome_spectral_indices.py
+++ .github/scripts/update_awesome_spectral_indices.py
@@ -9,3 +9,19 @@ awesomeSpectralIndices = requests.get(
 # Save the dict as json file
 with open("./spyndex/data/spectral-indices-dict.json", "w") as fp:
     json.dump(awesomeSpectralIndices, fp, indent=4, sort_keys=True)
+
+# Request Constants
+constants = requests.get(
+    "https://raw.githubusercontent.com/awesome-spectral-indices/awesome-spectral-indices/main/output/constants.json"
+).json()
+# Save the dict as json file
+with open("./spyndex/data/constants.json", "w") as fp:
+    json.dump(constants, fp, indent=4, sort_keys=True)
+
+# Request Bands
+bands = requests.get(
+    "https://raw.githubusercontent.com/awesome-spectral-indices/awesome-spectral-indices/main/output/bands.json"
+).json()
+# Save the dict as json file
+with open("./spyndex/data/bands.json", "w") as fp:
+    json.dump(bands, fp, indent=4, sort_keys=True)
