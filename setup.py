cbd68b8551c3580299c6d71c63eead1996466b2b
davemlz <dml.mont@gmail.com>
2023-03-13 10:33:34 +0100

    VERSION: 0.4.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(filename):
 
 setup(
     name="spyndex",
-    version="0.3.0",
+    version="0.4.0",
     url="https://github.com/awesome-spectral-indices/spyndex",
     license="MIT",
     author="David Montero Loaiza",
